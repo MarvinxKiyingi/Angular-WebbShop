33386c322e0aa38fb6503d83400327dad7bfb042
marv-ink <marvin96k@gmail.com>
2021-05-25 23:06:56 +0200

    Using the  collected data from the internet

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Movie } from 'src/app/model/Movie';
+import { MovieService } from 'src/app/service/movie.service';
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
-  constructor() {}
+  movies: Movie[] = [];
 
-  ngOnInit(): void {}
+  constructor(private service: MovieService) {}
+
+  ngOnInit(): void {
+    this.service.movies$.subscribe((data) => {
+      this.movies = data;
+
+      console.log(this.movies);
+    });
+
+    this.service.getMovies();
+  }
 }
