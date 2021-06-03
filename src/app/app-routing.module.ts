d93d9205e8aa0dafd04c380fcb849fcc342153ff
marv-ink <marvin96k@gmail.com>
2021-06-03 16:42:35 +0200

    movie img i now clickable to show more information

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/shopping-cart/cart/cart.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { MovieSpecificsComponent } from './components/movie-specifics/movie-specifics.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: MoviesComponent,
-  },
+  { path: '', component: MoviesComponent },
+  { path: 'movie/:id', component: MovieSpecificsComponent },
   { path: 'shoppingcart', component: CartComponent },
 ];
 
--- src/app/components/movie-specifics/movie-specifics.component.ts
+++ src/app/components/movie-specifics/movie-specifics.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Movie } from 'src/app/model/Movie';
+import { MovieService } from 'src/app/service/movie/movie.service';
 
 @Component({
   selector: 'app-movie-specifics',
   templateUrl: './movie-specifics.component.html',
-  styleUrls: ['./movie-specifics.component.scss']
+  styleUrls: ['./movie-specifics.component.scss'],
 })
 export class MovieSpecificsComponent implements OnInit {
-
-  constructor() { }
+  id: number = 0;
+  movieItem: Movie;
+  constructor(
+    private movieService: MovieService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.id = parseInt(params.get('id'));
+      this.movieItem = this.movieService.getMoviesId(this.id);
+    });
   }
-
+  handleBackToHome(): void {
+    this.router.navigate(['/']);
+    console.log('back tohome was clicked');
+  }
 }
